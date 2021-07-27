125cf727c53e1717624bae2bbb31e66963a2dc75
bae <baesj11@gmail.com>
2021-07-27 16:14:09 +0900

    sortparallelSort
    sort와 parallelSort에 대한 실습 추가

--- src/main/java/aboutsort/ExSort.java
+++ src/main/java/aboutsort/ExSort.java
@@ -3,24 +3,28 @@ package aboutsort;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class ExSort {
     public static void main(String[] args){
-        //1차원 배열 sort
+        //1차원 배열 오름차순 sort
         int[] nums = {9,8,1,3,2};
         Arrays.sort(nums);
         for(int i : nums){
-            System.out.println(i);
-        }
-        System.out.println("----------");
-        Integer[] nums2 = {9,8,1,3,2};
-        Arrays.sort(nums2, Collections.reverseOrder());
-        for(int i : nums2){
-            System.out.println(i);
+            System.out.print(i+" ");
         }
         System.out.println("----------");
 
-        //2차원 배열 sort
+        //1차원 배열 내림차순 sort
+        Integer[] nums2 = {9,8,1,3,2};
+        Arrays.sort(nums2, Collections.reverseOrder());
+        for(int i : nums2){
+            System.out.print(i+" ");
+        }
+        System.out.println("----------");
+
+        //2차원 배열 오름차순 sort
         int[][] numsArray = {{1,3},{1,2},{3,4},{2,2},{5,1}};
 //        Arrays.sort(numsArray, new Comparator<int[]>() { //@FunctionalInterface 함수형 인터페이스이다
 //            @Override
@@ -47,6 +51,7 @@ public class ExSort {
 
         System.out.println("----------");
 
+        //2차원 배열 내림차순 sort
         Arrays.sort(numsArray, (o1, o2) -> {
             if(o1[0] == o2[0]){
                 return Integer.compare(o2[1], o1[1]);
@@ -58,5 +63,21 @@ public class ExSort {
         for(int i=0; i<numsArray.length; i++){
             System.out.println(numsArray[i][0] + " " + numsArray[i][1]);
         }
+
+
+        //효율성은 같다 - 시간 O(nlogN) 공간 O(n)
+        int size = 1500;
+        int[] numbers = new int[size];
+        Random random = new Random();
+
+        IntStream.range(0,size).forEach(i -> numbers[i] = random.nextInt());
+        long start = System.nanoTime();
+        Arrays.sort(numbers); //DualPivotQuicksort
+        System.out.println("serial sorting took " + (System.nanoTime() - start));
+
+        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+        start = System.nanoTime();
+        Arrays.parallelSort(numbers); //Merge Sort
+        System.out.println("parallel sorting took " + (System.nanoTime() - start));
     }
 }
