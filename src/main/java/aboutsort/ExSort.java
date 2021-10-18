dc7189f9cc6cf60cbc59c9d54084f65a7cadcb33
bae <baesj11@gmail.com>
2021-10-18 17:30:11 +0900

    sort
    정렬하는 방법 추가 - compare, compareTo 사용

--- src/main/java/aboutsort/ExSort.java
+++ src/main/java/aboutsort/ExSort.java
@@ -1,12 +1,11 @@
 package aboutsort;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class ExSort {
     public static void main(String[] args){
+        System.out.println("1차원 배열 오름차순 sort");
         //1차원 배열 오름차순 sort
         int[] nums = {9,8,1,3,2};
         Arrays.sort(nums);
@@ -15,6 +14,47 @@ public class ExSort {
         }
         System.out.println("----------");
 
+
+
+
+
+        System.out.println("1차원 배열 오름차순 sort - compare 사용");
+        //1차원 배열 오름차순 sort - compare 사용
+        Integer[] nums3 = {9,8,1,3,2};
+        Arrays.sort(nums3, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1-o2;
+            }
+        });
+        for(int i : nums3){
+            System.out.print(i+" ");
+        }
+        System.out.println("----------");
+
+
+
+
+
+        System.out.println("1차원 배열 오름차순 sort - compareTo 사용");
+        //1차원 배열 오름차순 sort - compareTo 사용
+        Integer[] nums5 = {9,8,1,3,2};
+        Arrays.sort(nums5, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        for(int i : nums5){
+            System.out.print(i+" ");
+        }
+        System.out.println("----------");
+
+
+
+
+
+        System.out.println("1차원 배열 내림차순 sort");
         //1차원 배열 내림차순 sort
         Integer[] nums2 = {9,8,1,3,2};
         Arrays.sort(nums2, Collections.reverseOrder());
@@ -23,8 +63,80 @@ public class ExSort {
         }
         System.out.println("----------");
 
+
+
+
+
+        System.out.println("1차원 배열 내림차순 sort - compare 사용");
+        //1차원 배열 내림차순 sort - compare 사용
+        Integer[] nums4 = {9,8,1,3,2};
+        Arrays.sort(nums4, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        for(int i : nums4){
+            System.out.print(i+" ");
+        }
+        System.out.println("----------");
+
+
+
+
+
+        System.out.println("1차원 배열 내림차순 sort - compareTo 사용");
+        //1차원 배열 내림차순 sort - compareTo 사용
+        Integer[] nums6 = {9,8,1,3,2};
+        Arrays.sort(nums6, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        for(int i : nums6){
+            System.out.print(i+" ");
+        }
+        System.out.println("----------");
+
+
+
+
+
+        System.out.println("2차원 배열 오름차순 sort");
         //2차원 배열 오름차순 sort
+        int[][] numsArray2 = {{1,3},{1,2},{3,4},{2,2},{5,1}};
+//        Arrays.sort(numsArray2, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                if (o1[0] == o2[0]) {
+//                    return o1[1] - o2[1];
+//                } else {
+//                    return o1[0] - o2[0];
+//                }
+//            }
+//        });
+
+        Arrays.sort(numsArray2, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            } else {
+                return o1[0] - o2[0];
+            }
+        });
+
+        for(int i=0; i<numsArray2.length; i++){
+            System.out.println(numsArray2[i][0] + " " + numsArray2[i][1]);
+        }
+        System.out.println("----------");
+
+
+
+
+
+        System.out.println("2차원 배열 오름차순 sort - Integer.compare 사용");
         int[][] numsArray = {{1,3},{1,2},{3,4},{2,2},{5,1}};
+        //2차원 배열 오름차순 sort
 //        Arrays.sort(numsArray, new Comparator<int[]>() { //@FunctionalInterface 함수형 인터페이스이다
 //            @Override
 //            public int compare(int[] o1, int[] o2) {
@@ -47,10 +159,54 @@ public class ExSort {
         for(int i=0; i<numsArray.length; i++){
             System.out.println(numsArray[i][0] + " " + numsArray[i][1]);
         }
-
         System.out.println("----------");
 
+
+
+
+        System.out.println("2차원 배열 내림차순 sort");
         //2차원 배열 내림차순 sort
+        Arrays.sort(numsArray, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o2[1] - o1[1];
+                } else {
+                    return o2[0] - o1[0];
+                }
+            }
+        });
+
+        Arrays.sort(numsArray, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o2[1] - o1[1];
+            } else {
+                return o2[0] - o1[0];
+            }
+        });
+
+        for(int i=0; i<numsArray.length; i++){
+            System.out.println(numsArray[i][0] + " " + numsArray[i][1]);
+        }
+        System.out.println("----------");
+
+
+        
+
+        System.out.println("2차원 배열 내림차순 sort - Integer.compare 사용");
+        //2차원 배열 내림차순 sort
+//        Arrays.sort(numsArray, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                if (o1[0] == o2[0]) {
+//                    return Integer.compare(o2[1], o1[1]);
+//                } else {
+//
+//                    return Integer.compare(o2[0], o1[0]);
+//                }
+//            }
+//        });
+
         Arrays.sort(numsArray, (o1, o2) -> {
             if(o1[0] == o2[0]){
                 return Integer.compare(o2[1], o1[1]);
@@ -82,5 +238,7 @@ public class ExSort {
         start = System.nanoTime();
         Arrays.parallelSort(numbers); //Merge Sort
         System.out.println("parallel sorting took " + (System.nanoTime() - start));
+
     }
+
 }
